24e4eb5e256e7f80cd3f9fa62a1f8193f01fe53e
Patrick Roche <patrick.l.roche@gmail.com>
2015-10-15 20:15:58 -0400

    Update app.js to add moving enemies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Enemies our player must avoid
-var Enemy = function() {
+// row determines what row the Enemy will appear on
+// delay determines the delay before Enemy appears on the screen
+var Enemy = function(row, delay) {
     
     var obj = Object.create(Enemy.prototype);
      
@@ -8,8 +10,8 @@ var Enemy = function() {
     
 
     // column multipler = 101, row multiplier = 83
-    obj.x = 0*101;
-    obj.y = 5*83-20;
+    obj.x = (delay*-1)*101-101;
+    obj.y = row*83-20;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -24,6 +26,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + 500 * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -87,7 +90,12 @@ Player.prototype.handleInput = function(direction) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-allEnemies[0] = Enemy();
+allEnemies[0] = Enemy(1,0);
+allEnemies[1] = Enemy(2,1);
+allEnemies[2] = Enemy(3,2);
+allEnemies[3] = Enemy(1,5);
+allEnemies[4] = Enemy(2,6);
+allEnemies[5] = Enemy(3,8);
 var player = Player();
 
 
