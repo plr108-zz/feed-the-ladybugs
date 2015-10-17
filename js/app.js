7fc5a8e6f3112857e5aacfd4547661bb81b58621
Patrick Roche <patrick.l.roche@gmail.com>
2015-10-16 21:50:28 -0400

    Update app.js to add collisions

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 // Enemies our player must avoid
 // row determines what row the Enemy will appear on
 // delay determines the delay before Enemy appears on the screen
-var Enemy = function(row, delay) {
+var Enemy = function(row, delay, speed) {
     
     var obj = Object.create(Enemy.prototype);
      
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    
+    // we've provided one for you to get started    
+    var winning = false;
+    var losing = false;
 
     // column multipler = 101, row multiplier = 83
     obj.x = (delay*-1)*101-101;
     obj.y = row*83-20;
+    obj.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -26,7 +28,28 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + 500 * dt;
+    
+    if(this.x < 1000)
+    {
+        this.x = this.x + 200 * dt * this.speed;
+    }
+    else
+    {
+        // the Enenmy is off of the right side of the canvas.
+        // Reset the x position so the Enemy reappears on the left side
+        this.x = -110;
+    }
+
+    if(this.y === player.y - 10)
+    {
+        if(Math.abs(player.x - this.x) < 80) {
+            console.log("BUG TOUCH!");
+            player.resetPosition(dt);
+        }
+        else {
+            console.log("OK!");
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -41,6 +64,9 @@ var Player = function() {
     var obj = Object.create(Player.prototype);
     obj.sprite = 'images/char-boy.png';
     
+    // level is incremented every time a player makes it to the water
+    obj.level = 0;
+
     // column multipler = 101, row multiplier = 83
     obj.x = 2*101;
     obj.y = 5*83-10;
@@ -48,11 +74,32 @@ var Player = function() {
 };
 
 Player.prototype.update = function(dt) {
+    if(this.y === -10) {
+        this.level++;
+        console.log(allEnemies.length);
+        console.log("WINNING!  x: " + this.x);
+        if (this.x > -110) {
+            this.x = (this.x - 10);
+        }
+        else
+        {
+            this.resetPosition(dt);
+        }
+    }
 };
 
 Player.prototype.render = function(dt) {    
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Player.prototype.resetPosition = function(dt) {    
+    this.x = 2*101;
+    this.y = 5*83-10;
+    winning = false;
+    losing = false;    
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 
 Player.prototype.handleInput = function(direction) {
     switch(direction) {
@@ -79,7 +126,7 @@ Player.prototype.handleInput = function(direction) {
             if(this.x < 404) {
                 this.x = this.x + 101;
             }
-            console.log("x = " + this.x);
+            //console.log("x = " + this.x);
             break;
         default:
             // do nothing
@@ -90,12 +137,18 @@ Player.prototype.handleInput = function(direction) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-allEnemies[0] = Enemy(1,0);
-allEnemies[1] = Enemy(2,1);
-allEnemies[2] = Enemy(3,2);
-allEnemies[3] = Enemy(1,5);
-allEnemies[4] = Enemy(2,6);
-allEnemies[5] = Enemy(3,8);
+allEnemies[0] = Enemy(3,0,1);
+allEnemies[1] = Enemy(2,1,.75);
+allEnemies[2] = Enemy(3,2,.5);
+allEnemies[3] = Enemy(1,5,.75);
+allEnemies[4] = Enemy(3,6,1);
+allEnemies[5] = Enemy(2,8,.5);
+allEnemies[6] = Enemy(2,7.5,1);
+allEnemies[7] = Enemy(1,11,.25);
+allEnemies[8] = Enemy(3,12,.75);
+allEnemies[9] = Enemy(2,13,.5);
+allEnemies[10] = Enemy(3,15,.25);
+allEnemies[11] = Enemy(1,15.5,1);
 var player = Player();
 
 
